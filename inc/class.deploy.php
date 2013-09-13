ba70e0c1fe7739d08e0d09bcdada9f18e1b8b598
Joan Vega <joan@inte.es>
2013-09-13 14:12:19 +0200

    mod: preserving tab indentation

--- inc/class.deploy.php
+++ inc/class.deploy.php
@@ -1,7 +1,7 @@
 <?php
 // Causes the script to die if we are not using an actual enpoint to access it.
 if( ! defined( 'ACTIVE_DEPLOY_ENDPOINT' ) || true !== ACTIVE_DEPLOY_ENDPOINT )
-    die( '<h1>No Access</h1><p>An enpoint needs to be defined to use this file.</p>' );
+	die( '<h1>No Access</h1><p>An enpoint needs to be defined to use this file.</p>' );
 /**
  * The main Deploy class. This is set up for GIT repos.
  *
@@ -24,190 +24,189 @@ if( ! defined( 'ACTIVE_DEPLOY_ENDPOINT' ) || true !== ACTIVE_DEPLOY_ENDPOINT )
  */
 
 abstract class Deploy {
-    /**
-     * Registered deploy repos
-     */
-    protected static $repos = array();
+	/**
+	 * Registered deploy repos
+	 */
+	protected static $repos = array();
 
-    /**
-     * The name of the file that will be used for logging deployments. Set
-     * to false to disable logging.
-     */
-    private static $_log_name = 'deployments.log';
+	/**
+	 * The name of the file that will be used for logging deployments. Set 
+	 * to false to disable logging.
+	 */
+	private static $_log_name = 'deployments.log';
 
-    /**
-     * The path to where we wish to store our log file.
-     */
-    private static $_log_path = DEPLOY_LOG_DIR;
+	/**
+	 * The path to where we wish to store our log file.
+	 */
+	private static $_log_path = DEPLOY_LOG_DIR;
 
-    /**
-     * The timestamp format used for logging.
-     *
-     * @link    http://www.php.net/manual/en/function.date.php
-     */
-    private static $_date_format = 'Y-m-d H:i:sP';
-
-    /**
+	/**
+	 * The timestamp format used for logging.
+	 * 
+	 * @link    http://www.php.net/manual/en/function.date.php
+	 */
+	private static $_date_format = 'Y-m-d H:i:sP';
+	
+	/**
      * Registers available repos for deployment
-     *
+	 *
      * @param string $name The name of the repo we are attempting deployment for.
-     * @param array $repo The repo information and the path information for deployment
-     * @return bool True on success, false on failure.
-     */
-    public static function register_repo( $name, $repo ) {
-        if ( ! is_string( $name ) )
-            return false;
+	 * @param array $repo The repo information and the path information for deployment
+	 * @return bool True on success, false on failure.
+	 */
+	public static function register_repo( $name, $repo ) {
+		if ( ! is_string( $name ) )
+			return false;
 
-        if ( ! is_array( $repo ) )
-            return false;
+		if ( ! is_array( $repo ) )
+			return false;
+		
+		$required_keys = array( 'path', 'branch' );
+		foreach ( $required_keys as $key ) {
+			if ( ! array_key_exists( $key, $repo ) )
+				return false;
+		}
 
-        $required_keys = array( 'path', 'branch' );
-        foreach ( $required_keys as $key ) {
-            if ( ! array_key_exists( $key, $repo ) )
-                return false;
-        }
+		$defaults = array(
+			'remote'      => 'origin',
+			'post_deploy' => '',
+			'commit'      => '',
+		);
+		$repo = array_merge( $defaults, $repo );
 
-        $defaults = array(
-            'remote'      => 'origin',
-            'post_deploy' => '',
-            'commit'      => '',
-        );
-        $repo = array_merge( $defaults, $repo );
-
-        self::$repos[ $name ] = $repo;
+		self::$repos[ $name ] = $repo;
 
         return true;
-    }
+	}
 
-    /**
-     * Allows alternate log locations and date formats
-     *
+	/**
+	 * Allows alternate log locations and date formats
+	 *
      * @param $var
      * @param $value
-     * @return void.
-     */
-    public static function set( $var, $value ) {
-        if ( ( 'log_name' === $var || 'date_format' === $var ) && is_string( $value ) )
-            self::${'_'.$var} = $value;
-    }
+	 * @return void.
+	 */
+	public static function set( $var, $value ) {
+		if ( ( 'log_name' === $var || 'date_format' === $var ) && is_string( $value ) )
+			self::${'_'.$var} = $value;
+	}
 
-    /**
-     * Whether or not we are ready to deploy
-     */
-    private $_deploy_ready;
+	/**
+	 * Whether or not we are ready to deploy
+	 */
+	private $_deploy_ready;
 
-    /**
-     * The name of the repo we are attempting deployment for.
-     */
-    private $_name;
+	/**
+	 * The name of the repo we are attempting deployment for.
+	 */
+	private $_name;
 
-    /**
-     * The name of the branch to pull from.
-     */
-    private $_branch;
+	/**
+	 * The name of the branch to pull from.
+	 */
+	private $_branch;
 
-    /**
-     * The name of the remote to pull from.
-     */
-    private $_remote;
+	/**
+	 * The name of the remote to pull from.
+	 */
+	private $_remote;
 
-    /**
-     * The path to where your website and git repository are located, can be
-     * a relative or absolute path
-     */
-    private $_path;
+	/**
+	 * The path to where your website and git repository are located, can be 
+	 * a relative or absolute path
+	 */
+	private $_path;
 
-    /**
-     * A callback function to call after the deploy has finished.
-     */
-    private $_post_deploy;
+	/**
+	 * A callback function to call after the deploy has finished.
+	 */
+	private $_post_deploy;
 
-    /**
-     * The commit that we are attempting to deploy
-     */
-    private $_commit;
+	/**
+	 * The commit that we are attempting to deploy
+	 */
+	private $_commit;
 
-
-    /**
+	/**
      * Info sent to stdout & stderr when executing commands
      */
     private $_command_output = array();
 
     /**
-     * Sets up the repo information.
-     *
+	 * Sets up the repo information.
+	 * 
      * @param string $name The name of the repo we are attempting deployment for.
-     * @param array $repo The repository info. See class block for docs.
-     */
-    protected function __construct( $name, $repo ) {
-        $this->_path = realpath( $repo['path'] ) . DIRECTORY_SEPARATOR;
+	 * @param 	array 	$repo 	The repository info. See class block for docs.
+	 */
+	protected function __construct( $name, $repo ) {
+		$this->_path = realpath( $repo['path'] ) . DIRECTORY_SEPARATOR;
 
-        $this->_name = $name;
+		$this->_name = $name;
 
-        $available_options = array( 'branch', 'remote', 'commit', 'post_deploy' );
+		$available_options = array( 'branch', 'remote', 'commit', 'post_deploy' );
 
-        foreach ( $repo as $option => $value ){
-            if ( in_array( $option, $available_options ) ){
-                $this->{'_'.$option} = $value;
-            }
-        }
+		foreach ( $repo as $option => $value ){
+			if ( in_array( $option, $available_options ) ){
+				$this->{'_'.$option} = $value;
+			}
+		}
 
-        $this->execute();
-    }
+		$this->execute();
+	}
 
-    /**
-     * Writes a message to the log file.
-     *
-     * @param 	string 	$message 	The message to write
-     * @param 	string 	$type 		The type of log message (e.g. INFO, DEBUG, ERROR, etc.)
-     */
-    protected function log( $message, $type = 'INFO' ) {
-        if ( self::$_log_name ) {
-            // Set the name of the log file
-            $filename = self::$_log_path . '/' . rtrim( self::$_log_name, '/' );
+	/**
+	 * Writes a message to the log file.
+	 * 
+	 * @param 	string 	$message 	The message to write
+	 * @param 	string 	$type 		The type of log message (e.g. INFO, DEBUG, ERROR, etc.)
+	 */
+	protected function log( $message, $type = 'INFO' ) {
+		if ( self::$_log_name ) {
+			// Set the name of the log file
+			$filename = self::$_log_path . '/' . rtrim( self::$_log_name, '/' );
 
-            if ( ! file_exists( $filename ) ) {
-                // Create the log file
-                file_put_contents( $filename, '' );
+			if ( ! file_exists( $filename ) ) {
+				// Create the log file
+				file_put_contents( $filename, '' );
 
-                // Allow anyone to write to log files
-                chmod( $filename, 0666 );
-            }
+				// Allow anyone to write to log files
+				chmod( $filename, 0666 );
+			}
 
-            // Write the message into the log file
-            // Format: time --- type: message
-            file_put_contents( $filename, date( self::$_date_format ) . ' --- ' . $type . ': ' . $message . PHP_EOL, FILE_APPEND );
-        }
-    }
+			// Write the message into the log file
+			// Format: time --- type: message
+			file_put_contents( $filename, date( self::$_date_format ) . ' --- ' . $type . ': ' . $message . PHP_EOL, FILE_APPEND );
+		}
+	}
 
-    /**
-     * Executes the necessary commands to deploy the code.
-     */
-    private function execute() {
-        try {
-            // Make sure we're in the right directory
+	/**
+	* Executes the necessary commands to deploy the code.
+	*/
+	private function execute() {
+		try {
+			// Make sure we're in the right directory
             $result = chdir( $this->_path);
             if ($result !== true) throw new Exception('error on chdir');
 
-            // Discard any changes to tracked files since our last deploy
+			// Discard any changes to tracked files since our last deploy
             $this->executeCommand('git reset --hard HEAD');
 
-            // Update the local repository
+			// Update the local repository
             $this->executeCommand('git pull ' . $this->_remote . ' ' . $this->_branch);
 
-            // Secure the .git directory
+			// Secure the .git directory
             $this->executeCommand('chmod -R og-rx .git');
 
-            if ( is_callable( $this->_post_deploy ) )
-                call_user_func( $this->_post_deploy );
+			if ( is_callable( $this->_post_deploy ) )
+				call_user_func( $this->_post_deploy );
 
-            $this->log( '[SHA: ' . $this->_commit . '] Deployment of ' . $this->_name . ' from branch ' . $this->_branch . ' successful');
+			$this->log( '[SHA: ' . $this->_commit . '] Deployment of ' . $this->_name . ' from branch ' . $this->_branch . ' successful' );
             $this->log( PHP_EOL.implode(PHP_EOL, $this->_command_output), 'DEBUG');
 
-        } catch ( Exception $e ) {
-            $this->log( $e, 'ERROR' );
-        }
-    }
+		} catch ( Exception $e ) {
+			$this->log( $e, 'ERROR' );
+		}
+	}
 
     /**
      * @param string $command The command to execute
@@ -223,4 +222,4 @@ abstract class Deploy {
 }
 // Registers all of our repos with the Deploy class
 foreach ( $repos as $name => $repo )
-    Deploy::register_repo( $name, $repo );
+	Deploy::register_repo( $name, $repo );
